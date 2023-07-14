6d01428781cc4196220e76d6ea3a5e16aa20da15
ThomasWaytec <thomas.waytec@gmail.com>
2023-07-14 19:57:00 +0200

    Add relative filepath

--- src/minimalist_hex_viewer.c
+++ src/minimalist_hex_viewer.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
 
 
-const char FILEPATH[] = "/test_files/test_1.jpg";
+const char FILEPATH[] = "..\\test_files\\jpeg\\test_1.jpg";
 
 int main(void) {
 
 
+    FILE* file = fopen(FILEPATH, "r");
 
 
+
+    char hex_value;
+    do {
+        hex_value = fgetc(file);
+        printf("%c\n", hex_value);
+        printf("%X", EOF);
+
+    } while (hex_value != EOF);
+    printf("Loop terminated!");
 
     
     
