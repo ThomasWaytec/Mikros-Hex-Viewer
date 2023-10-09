16fccdfb464f66669c4a0674fdb39253be38b97b
ThomasWaytec <thomas.waytec@gmail.com>
2023-10-09 17:03:46 +0200

    create functionality to view decimals in a separate file

---
+++ src/decimal_viewer.c
@@ -0,0 +1,165 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#ifdef _WIN32
+    #include <Windows.h>
+    
+    size_t get_terminal_width() {
+        HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+        CONSOLE_SCREEN_BUFFER_INFO csbi;
+
+        GetConsoleScreenBufferInfo(hConsole, &csbi);
+        size_t terminal_width = csbi.srWindow.Right - csbi.srWindow.Left + 1;
+
+        return terminal_width;
+    }
+
+#endif
+
+#ifdef linux
+    #include <sys/ioctl.h>
+    #include <unistd.h>
+    
+    size_t get_terminal_width() {
+        struct winsize ws;
+        
+        ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws);
+        size_t terminal_width = ws.ws_col;
+
+        return terminal_width;
+    }
+#endif
+
+#define MAX_LINE_LEN get_terminal_width()
+
+/* most macros are defined as doubles for easier calculations further on */
+#define PCT_SIGN_LEN 1.0    /* PCT = PERCENTAGE */
+#define COMPLETION_PCT_LEN (3.0 + PCT_SIGN_LEN)
+
+#define SPACE_LEN 1.0
+#define NEW_LINE_LEN 1.0
+#define VERTICAL_LINE_LEN 1.0
+
+#define RAW_DEC_LEN 3   /* max. possible length of a byte represented in decimal */                                
+                        /* raw, as in they aren't formatted */
+
+#define DEC_LEN (RAW_DEC_LEN + SPACE_LEN)                               
+#define DEC_GROUP_SIZE 4.0                                  /* the number offormatted dec values in one group */
+#define DEC_GROUP_LEN (DEC_LEN*DEC_GROUP_SIZE + SPACE_LEN)  /* how many characters a formatted dec group takes up */
+
+
+
+
+
+void fatal_error(const char* message)
+{
+    fprintf(stderr,"\nError: %s\n", message);
+    exit(EXIT_FAILURE);
+}
+
+size_t int_len(size_t number) {
+    if (number == 0) {return 1;}
+
+    return floor(log10(abs(number))) + 1;
+}
+
+size_t doube_len(double number) {
+    if (number == 0) {return 1;}
+
+    return floor(log10(abs(number))) + 1;
+
+}
+
+void print_raw_decimal(unsigned int value) {
+
+    printf("%0*d", RAW_DEC_LEN, value);
+}
+
+
+const size_t get_file_size(FILE* file) {
+
+    fseek(file, 0, SEEK_END);
+    const size_t FILE_SIZE = ftell(file);            
+    fseek(file, 0, SEEK_SET);
+
+    return FILE_SIZE;
+}
+
+void print_header(size_t current_line, double lines, double dec_per_line) {
+        size_t completion_pct = (current_line + 1)/lines*100;
+        size_t completion_pct_padding = COMPLETION_PCT_LEN - PCT_SIGN_LEN;
+
+        size_t dec_printed = dec_per_line*(double)current_line;
+        size_t dec_printed_padding = int_len(dec_per_line*lines);
+
+
+        printf("%0*u%% %0*u| ", completion_pct_padding, completion_pct, dec_printed_padding, dec_printed);
+
+}
+
+
+
+int main(int argc, char* argv[]) {
+
+
+    /* validate coommand-line arguments count */
+    if ((argc - 1) != 1) {
+        char* argc_buffer = calloc(sizeof(char), 54 + int_len(argc) + 1);
+        sprintf(argc_buffer, "Program takes 1 command-line argument but %d were given.", argc - 1);
+        fatal_error(argc_buffer);
+    }
+
+    const char* FILEPATH = argv[1];
+    /* try to open file */
+    FILE* file = fopen(FILEPATH, "rb");
+    if (file == NULL) {fatal_error("Cannot find or open file.");}
+
+
+
+    const size_t FILE_SIZE = get_file_size(file);
+
+
+    /* defined as double for easier calculations */
+    double header_len = COMPLETION_PCT_LEN + SPACE_LEN + int_len(FILE_SIZE) + VERTICAL_LINE_LEN + SPACE_LEN;
+    double payload_len = MAX_LINE_LEN - header_len;
+    
+    double dec_groups_per_line = floor((payload_len - NEW_LINE_LEN)/DEC_GROUP_LEN);
+    double dec_per_line = floor(dec_groups_per_line*DEC_GROUP_SIZE);
+
+    size_t lines = (size_t)ceil((double)FILE_SIZE/dec_per_line);
+
+
+
+
+    /* set up stdout full buffering */
+    void* stdout_buffer = NULL;
+    setvbuf(stdout, stdout_buffer, _IOFBF, INT_MAX);
+
+
+
+
+
+    /* read and print content of file */
+    unsigned int dec; // has to be int to be able to check for EOF
+    for (size_t line = 0; line < lines; line++)
+    {
+        print_header(line, lines, dec_per_line);
+
+        for (size_t _ = 0; _ < dec_groups_per_line; _++)
+        {
+
+            for (size_t _ = 0; _ < DEC_GROUP_SIZE && (dec = fgetc(file)) != EOF; _++) 
+            {
+                print_raw_decimal(dec);
+                printf(" ");
+            }
+            printf(" ");
+            
+        }       
+        printf("\n");
+    }
+
+    fclose(file);
+    return 0;   
+}
