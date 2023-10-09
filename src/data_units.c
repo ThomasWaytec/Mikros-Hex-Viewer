d0afedc1b73c3494a09eeec3f901952cf97234b4
ThomasWaytec <thomas.waytec@gmail.com>
2023-10-09 23:14:00 +0200

    initialize struct instances outside of main function

--- src/data_units.c
+++ src/data_units.c
@@ -3,36 +3,32 @@
 #include "char_lengths.h"
 #include "data_units.h"
 
+/* binary */
+DataUnit bin = {
+    .raw_unit_len = 8,
+    .raw_unit_padding = 8.0,
 
-int main(void) 
-{
-    /* binary */
-    extern DataUnit bin;
-    bin.raw_unit_len = 8;
-    bin.raw_unit_padding = 8.0;
+    .unit_len = 8 + SPACE_LEN,              /* .raw_unit_len + SPACE_LEN */
+    .unit_group_size = 1.0,
+    .unit_group_len = 9.0*1.0 + SPACE_LEN   /* .unit_len*.unit_group_size + SPACE_LEN */ 
+};
 
-    bin.unit_len = bin.raw_unit_len + SPACE_LEN;
-    bin.unit_group_size = 1.0;
-    bin.unit_group_len = bin.unit_len*bin.unit_group_size + SPACE_LEN;
+/* hexadecimal */
+DataUnit hex = {
+    .raw_unit_len = 2,
+    .raw_unit_padding = 2.0,
 
+    .unit_len = 2 + SPACE_LEN,              /* .raw_unit_len + SPACE_LEN */
+    .unit_group_size = 4.0,
+    .unit_group_len = 3.0*4.0 + SPACE_LEN   /* .unit_len*.unit_group_size + SPACE_LEN */ 
+};
 
-    /* hexadecimal */
-    extern DataUnit hex;
-    hex.raw_unit_len = 2;
-    hex.raw_unit_padding = 2.0;
+/* decimal */
+DataUnit dec = {
+    .raw_unit_len = 3,
+    .raw_unit_padding = 3.0,
 
-    hex.unit_len = binary.raw_unit_len + SPACE_LEN;
-    hex.unit_group_size = 4.0;
-    hex.unit_group_len = binary.unit_len*binary.unit_group_size + SPACE_LEN;
-
-
-    /* decimal */
-    extern DataUnit dec;
-    dec.raw_unit_len = 3;
-    dec.raw_unit_padding = 3.0;
-
-    dec.unit_len = binary.raw_unit_len + SPACE_LEN;
-    dec.unit_group_size = 3.0;
-    dec.unit_group_len = binary.unit_len*binary.unit_group_size + SPACE_LEN;
-    
-}
+    .unit_len = 3 + SPACE_LEN,              /* .raw_unit_len + SPACE_LEN */
+    .unit_group_size = 3.0,
+    .unit_group_len = 4.0*3.0 + SPACE_LEN   /* .unit_len*.unit_group_size + SPACE_LEN */ 
+};
