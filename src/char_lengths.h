e3257108422e0be9c6ecfb933bd080d3e11fc5d1
ThomasWaytec <thomas.waytec@gmail.com>
2023-10-09 20:31:58 +0200

    Make char_lengths.h to store character length constants for readability

---
+++ src/char_lengths.h
@@ -0,0 +1,11 @@
+/* define the lengths of characters to avoid usage of magic numbers and improve readability */
+
+/* (The macros are defined as double to facilitate calculations, but they are intended to store whole numbers.) */
+#ifndef CHAR_LENGTHS_H
+#define CHAR_LENGTHS_H
+
+#define SPACE_LEN 1.0
+#define NEW_LINE_LEN 1.0
+#define VERTICAL_LINE_LEN 1.0
+
+#endif
