8ccb8e806b05560027aa49fda48a5c7d59e8e730
ThomasWaytec <thomas.waytec@gmail.com>
2023-10-09 22:23:03 +0200

    Create binary, hexadecimal and decimal DataUnit instances

---
+++ src/data_units.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+
+#include "char_lengths.h"
+#include "data_units.h"
+
+
+int main(void) 
+{
+    /* binary */
+    extern DataUnit bin;
+    binary.raw_unit_len = 8;
+    binary.raw_unit_padding = 8.0;
+
+    binary.unit_len = binary.raw_unit_len + SPACE_LEN;
+    binary.unit_group_size = 1.0;
+    binary.unit_group_len = binary.unit_len*binary.unit_group_size + SPACE_LEN;
+
+
+    /* hexadecimal */
+    extern DataUnit hex;
+    hex.raw_unit_len = 2;
+    hex.raw_unit_padding = 2.0;
+
+    hex.unit_len = binary.raw_unit_len + SPACE_LEN;
+    hex.unit_group_size = 4.0;
+    hex.unit_group_len = binary.unit_len*binary.unit_group_size + SPACE_LEN;
+
+
+    /* decimal */
+    extern DataUnit dec;
+    dec.raw_unit_len = 3;
+    dec.raw_unit_padding = 3.0;
+
+    dec.unit_len = binary.raw_unit_len + SPACE_LEN;
+    dec.unit_group_size = 3.0;
+    dec.unit_group_len = binary.unit_len*binary.unit_group_size + SPACE_LEN;
+    
+}
