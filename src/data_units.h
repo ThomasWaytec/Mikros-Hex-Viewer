f08e848c23fb89d5f775649383a2f1bbce929f7f
ThomasWaytec <thomas.waytec@gmail.com>
2023-10-09 21:54:38 +0200

    Add further documentation

--- src/data_units.h
+++ src/data_units.h
@@ -9,12 +9,12 @@ but they are intended to store whole numbers.
 */
 typedef struct data_unit
 {
-    size_t raw_unit_len;
-    double raw_unit_padding;
+    size_t raw_unit_len;        /* max. possible length of a byte represented in the given data representation */
+    double raw_unit_padding;    /* raw, as in it isn't formatted */
     
-    double unit_len;
-    double unit_group_size;
-    double unit_group_len;
+    double unit_len;            /* the length of a formatted unit */
+    double unit_group_size;     /* the number of formatted units in one unit group */
+    double unit_group_len;      /* how many characters a formatted unit group takes up in total */
 
 } DataUnit;
 
