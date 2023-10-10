3b6e708dacb8390ba534ef6cd784bcfb4b1e371c
ThomasWaytec <thomas.waytec@gmail.com>
2023-10-10 19:23:18 +0200

    change raw_padding from double to size_t

--- src/data_formats.c
+++ src/data_formats.c
@@ -8,29 +8,29 @@ const char SUPPORTED_DATA_FORMATS[] = {'h', 'b', 'd'};
 /* hexadecimal */
 DataFormat hex = {
     .raw_len = 2,
-    .raw_padding = 2.0,
+    .raw_padding = 2,
 
-    .len = 2 + SPACE_LEN,              /* .raw_len + SPACE_LEN */
+    .len = 2.0 + SPACE_LEN,             /* .raw_len + SPACE_LEN */
     .group_size = 4.0,
-    .group_len = 3.0*4.0 + SPACE_LEN   /* .len*.group_size + SPACE_LEN */ 
+    .group_len = 3.0*4.0 + SPACE_LEN    /* .len*.group_size + SPACE_LEN */ 
 };
 
 /* binary */
 DataFormat bin = {
     .raw_len = 8,
-    .raw_padding = 8.0,
+    .raw_padding = 8,
 
-    .len = 8 + SPACE_LEN,              /* .raw_len + SPACE_LEN */
+    .len = 8.0 + SPACE_LEN,             /* .raw_len + SPACE_LEN */
     .group_size = 1.0,
-    .group_len = 9.0*1.0 + SPACE_LEN   /* .len*.group_size + SPACE_LEN */ 
+    .group_len = 9.0*1.0 + SPACE_LEN    /* .len*.group_size + SPACE_LEN */ 
 };
 
 /* decimal */
 DataFormat dec = {
     .raw_len = 3,
-    .raw_padding = 3.0,
+    .raw_padding = 3,
 
-    .len = 3 + SPACE_LEN,              /* .raw_len + SPACE_LEN */
+    .len = 3.0 + SPACE_LEN,             /* .raw_len + SPACE_LEN */
     .group_size = 3.0,
-    .group_len = 4.0*3.0 + SPACE_LEN   /* .len*.group_size + SPACE_LEN */ 
+    .group_len = 4.0*3.0 + SPACE_LEN    /* .len*.group_size + SPACE_LEN */ 
 };
--- src/data_formats.h
+++ src/data_formats.h
@@ -10,7 +10,7 @@ but they are intended to store whole numbers.
 typedef struct data_format
 {
     size_t raw_len;        /* max. possible length of a byte represented in the given data format/representation */
-    double raw_padding;    /* raw, as in it isn't styled/formatted */
+    size_t raw_padding;    /* raw, as in it isn't styled/formatted */
     
     double len;            /* the length (in characters) of a styled data format */
     double group_size;     /* the number of styled data formats in one group */
