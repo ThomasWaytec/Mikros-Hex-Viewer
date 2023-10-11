5fa36ae206353496663141546d98494a74608066
ThomasWaytec <thomas.waytec@gmail.com>
2023-10-11 02:15:03 +0200

    change data_formats

--- src/data_unit.h
+++ src/data_unit.h
@@ -1,7 +1,6 @@
 #ifndef DATA_UNIT_H
 #define DATA_UNIT_H
 
-#define DATA_FORMATS_LEN 8
 /*
 A data unit (data_unit_t) is a composite of one or many data formats (data_format_t).
 It is a logical unit, an added layer of abstraction so the main file (mikros_hex_viewer.c)
@@ -14,7 +13,8 @@ but they are intended to store whole numbers.
 */
 typedef struct data_unit {
 
-    char data_formats[DATA_FORMATS_LEN];   /* the type of data formats the data unit is made up of */
+    char* data_formats;   /* the type of data formats the data unit is made up of */
+    const size_t data_formats_len;
     double len;             /* the sum of all the data formats' lengths in .data_formats */
 
     double group_size;      /* the number data units in one data unit group */
