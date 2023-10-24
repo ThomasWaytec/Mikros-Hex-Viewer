2c6c8b6375ec29dcd1c8e22c6bd3dcf4c5c9731d
ThomasWaytec <thomas.waytec@gmail.com>
2023-10-24 23:15:26 +0200

    Update help message

--- src/help.c
+++ src/help.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 
-const char *help_message = "View the hexadecimal representation of a file in the terminal.\n" 
+const char *help_message = "mikros: View and analyze files in various formats (hexadecimal, binary, decimal, and character) with this hex viewer.\n" 
     "\n"
     "Usage: mikros FILE [OPTIONS]\n"
     "\n"
-    "Options:\n"
-    "  -x               Display data in hexadecimal format (default).\n"
-    "  -d               Display data in decimal format.\n"
-    "  -b               Display data in binary format.\n"
-    "  -c               Display data as characters.\n"
-    "  -h, --help       Display this help message and exit.\n"
-    "  --buffer         Enable stdout buffering for faster output (no Ctrl+C interruption).\n"
+    "Positional Arguments:\n"
+    "  FILE            The name or path of the file for analysis.\n"
+    "\n"
+    "OPTIONS:\n"
+    "  -h, --help      Display this help message and exit.\n"
+    "  -x              Display data in hexadecimal format (default).\n"
+    "  -d              Display data in decimal format.\n"
+    "  -b              Display data in binary format.\n"
+    "  -c              Display data as characters.\n"
+    "  --buffer        Enable stdout buffering for faster output (no Ctrl+C interruption).\n"
+    "\n"
     "Data format options can be combined without double dashes, e.g., -dxcb for decimal, hexadecimal, character and binary format.\n"
     "\n"
-    "Examples:\n"
+    "Example Usage:\n"
     "   mikros file.jpeg\n"
     "   mikros file.bin -dxcb\n"
     "   mikros -d --buffer folder/file.txt -xb\n";
@@ -21,5 +25,5 @@ const char *help_message = "View the hexadecimal representation of a file in the
 
 void print_help_message(void) {
 
-    printf("%s\n", help_message);   
+    printf("%s\n", help_message);
 }
